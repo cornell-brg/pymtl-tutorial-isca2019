4b225900a54187e4a2fece9e80c2009f42d01954
cb535 <cb535@cornell.edu>
2019-06-19 16:39:26 -0400

    staff update from: pymtl-tutorial-isca2019-0.1-341-g016aef7 [ci skip]

---
+++ examples/ex01_basics/incr_test.py
@@ -0,0 +1,17 @@
+"""
+==========================================================================
+incr_test.py
+==========================================================================
+An increment python function that uses PyMTL bits.
+
+Author : Yanghui Ou
+  Date : June 17, 2019
+"""
+from __future__ import absolute_import, division, print_function
+
+from pymtl3 import *
+
+# ''' TUTORIAL TASK ''''''''''''''''''''''''''''''''''''''''''''''''''''''
+# Implement the incr_8bit function and a corresponding unit test
+# ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
